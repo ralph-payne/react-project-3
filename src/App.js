4120638f94b13c597664b1a81e48e913598f7f5a
ralph-payne <ralphpayne@gmail.com>
2020-03-03 10:52:24 +0000

    Set up routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { Fragment, Component } from 'react';
+// You import the BrowserRouter and you give it an alias (Router)
+// In order to actually use the router, you need to go down to render() and wrap everything in the <Router> tag
+// We want to wrap our routes in a Switch so that it shows one at a time
+// Next step is creating a new folder in components where you can store the pages
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import About from './components/pages/About';
 import './App.css';
 
-function App() {
-    return (
-      <div className="App">
-          <h1>React Project 3</h1>
-      </div>
-    );
-  }
+class App extends Component {
+
+    state = {
+        loading: false
+    }
+
+    render() {
+
+    const { loading } = this.state;
+
+        return (
+
+            <Router>
+                <div className='App'>
+
+                <Navbar />
+
+                <div className='container'>
+
+                <Switch>
+
+                <Route exact path='/about' component={About} />
+
+                </Switch>
+
+                </div>
+                </div>
+
+            </Router>
+
+        )
+
+    }
+
+}
 
 export default App;
