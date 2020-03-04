1268bcef92a0a25f850ff786f376dd081c43863c
ralph-payne <ralphpayne@gmail.com>
2020-03-04 10:11:09 +0000

    Update comp names

--- src/App.js
+++ src/App.js
@@ -4,44 +4,40 @@ import React, { Fragment, Component } from 'react';
 // We want to wrap our routes in a Switch so that it shows one at a time
 // Next step is creating a new folder in components where you can store the pages
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
+import Navbar from './components/navbar/Navbar';
 import About from './components/pages/About';
+import CreateSurvey from './components/views/CreateSurvey';
+import ViewSurvey from './components/views/ViewSurvey';
 import './App.css';
 
 class App extends Component {
 
     state = {
-        loading: false
+        loading: false,
+        loggedInUser: null
     }
 
     render() {
 
-    const { loading } = this.state;
+    const { loading, loggedInUser } = this.state;
 
         return (
 
             <Router>
                 <div className='App'>
-
-                <Navbar />
-
-                <div className='container'>
-
-                <Switch>
-
-                <Route exact path='/about' component={About} />
-
-                </Switch>
-
+                    <Navbar />
+                        <div className='container'>
+                            <Switch>
+                                <Route exact path="/create-survey" component={CreateSurvey}/>
+                                {/* Eventually, you will have the view-survey route contain the id of the survey */}
+                                <Route exact path="/view-survey" component={ViewSurvey}/>
+                                <Route exact path='/about' component={About} />
+                            </Switch>
+                        </div>
                 </div>
-                </div>
-
             </Router>
-
         )
-
     }
-
 }
 
 export default App;
